eaacd01da72c45f0f8f9bc3481f5b83296066d2b
practice-repo <amangupta9039093489@gmail.com>
2024-02-25 10:34:30 +0530

    some file to add

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import './App.css'
 function App() {
   
 
-  console.log(process.env.REACT_APP_APPWRITE_URL);
+
+  console.log(import.meta.env.VITE_APPWRITE_URL) 
   return (
     <>
       <h1>blog write app </h1>
