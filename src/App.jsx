686e9fd9329f6c7e6f585041afda2f1eb20fa446
practice-repo <amangupta9039093489@gmail.com>
2024-02-25 10:06:28 +0530

    some file to add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
-  console.log(process.njt.REACT_APP_APPWRITE_URL);
+  console.log(process.env.REACT_APP_APPWRITE_URL);
   return (
     <>
       <h1>blog write app </h1>
